19d5920f5c5c09d9c5e104c2cb140263f5353848
Leblanc Charles <charles.leblanc@epitech.eu>
2021-11-21 21:53:38 +0100

    [ADD] few more test

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -14,31 +14,33 @@ import {
 
 import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 
 @Component
 export default  class Index extends Vue {
   objP: any= undefined;
-
+  scene: any= undefined;
   three() {
-      const scene = new THREE.Scene();
+      this.scene = new THREE.Scene();
 			const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
       const loader = new GLTFLoader();
-    
+      const colorBackground = new THREE.Color(0x5D3FD3);
+      this.scene.background = colorBackground;
       const light = new THREE.PointLight( 0xffffff, 1, 200 );
-      light.position.set( 0, 0, 0 );
-      scene.add( light );
+      light.position.set( 0, 10, 0 );
+      this.scene.add( light );
 
       const alight = new THREE.AmbientLight( 0xffffff, 1);
       alight.position.set( 0, 0, 0 );
-      scene.add( alight );
+      this.scene.add( alight );
 
       const alight2 = new THREE.PointLight( 0xffffff, 3, 200 )
-      alight2.position.set( 0, 0, 5 );
-      scene.add( alight2 );
+      alight2.position.set( 10, 10, 5 );
+      this.scene.add( alight2 );
 
       loader.load(
       // resource URL
-      "gamerChair.gltf",
+      "bedroom.gltf",
 
       // onLoad callback
       // Here the loaded data is assumed to be an object
@@ -48,7 +50,7 @@ export default  class Index extends Vue {
         this.objP = obj.scene;
         this.objP.position.y = -1.5;
         // Add the loaded object to the scene
-        scene.add( this.objP );
+        this.scene.add( this.objP );
       },
 
       // onProgress callback
@@ -64,6 +66,9 @@ export default  class Index extends Vue {
     );
       
 			const renderer = new THREE.WebGLRenderer();
+
+      const controls = new OrbitControls(camera, renderer.domElement);
+      controls.update();
 			renderer.setSize( window.innerWidth, window.innerHeight );
 			document.body.appendChild( renderer.domElement );
 
@@ -72,7 +77,10 @@ export default  class Index extends Vue {
 			const cube = new THREE.Mesh( geometry, material );
 			//scene.add( cube );
 
-			camera.position.z = 5;
+			camera.position.z = -7;
+      camera.position.y = 7;
+      camera.position.x = 10;
+      camera.lookAt(0, 0, 0);
 
 			const animate = () => {
 				requestAnimationFrame( animate );
@@ -80,12 +88,12 @@ export default  class Index extends Vue {
 				//cube.rotation.x += 0.01;
 				//cube.rotation.y += 0.01;
         if (this.objP) {
-          this.objP.rotation.y += 0.01;
+          this.objP.rotation.y += 0.001;
           //this.objP.rotation.x += 0.01;
         }
         
 
-				renderer.render( scene, camera );
+				renderer.render(this.scene, camera );
 			};
 
 			animate();
