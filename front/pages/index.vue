24946bd650ab5cc40bedec40e1949ed352048b50
Leblanc Charles <charles.leblanc@epitech.eu>
2021-11-25 03:07:09 +0100

    [ADD] outliner to scene object

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -16,7 +16,14 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
 import { RoomEnvironment } from 'three/examples/jsm/environments/RoomEnvironment.js';
-
+import { OutlinePass } from 'three/examples/jsm/postprocessing/OutlinePass.js';
+import { EffectComposer } from 'three/examples/jsm/postprocessing/EffectComposer.js';
+import { FXAAShader } from 'three/examples/jsm/shaders/FXAAShader.js';
+import { RenderPass } from 'three/examples/jsm/postprocessing/RenderPass.js';
+import { ShaderPass } from 'three/examples/jsm/postprocessing/ShaderPass.js';
+import { LuminosityShader } from 'three/examples/jsm/shaders/LuminosityShader.js';
+import { UnrealBloomPass } from 'three/examples/jsm/postprocessing/UnrealBloomPass.js';
+  
 @Component
 export default  class Index extends Vue {
   room: any= undefined;
@@ -25,6 +32,14 @@ export default  class Index extends Vue {
   renderer: any = undefined;
   loaderModel: any= undefined;
   controls: any = undefined;
+  raycaster: any = undefined;
+  mouse: any = undefined;
+  composer: any = undefined; 
+  outlinePass: any = undefined;
+  selectedObjects:any[] = [];
+
+  lightOffice: any= undefined;
+  
 
   initObject() {
     this.scene = new THREE.Scene();
@@ -32,17 +47,43 @@ export default  class Index extends Vue {
     this.loaderModel = new GLTFLoader();
     this.renderer = new THREE.WebGLRenderer({ antialias: true });
     this.controls = new OrbitControls(this.camera, this.renderer.domElement);
-    const raycaster = new THREE.Raycaster();
-    const mouse = new THREE.Vector2();
-
+    this.raycaster = new THREE.Raycaster();
+    this.mouse = new THREE.Vector2();
+    this.composer = new EffectComposer(this.renderer);
+    const pmremGenerator = new THREE.PMREMGenerator(this.renderer);
 
     this.controls.target.set( 0, 2, 0 );
     this.controls.update();
     this.controls.enablePan = false;
 		this.controls.enableDamping = true;
     this.renderer.outputEncoding = THREE.sRGBEncoding;
-    this.renderer.setSize( window.innerWidth, window.innerHeight );
+    this.renderer.setSize(window.innerWidth, window.innerHeight);
+    this.scene.environment = pmremGenerator.fromScene(new RoomEnvironment(), 0.04 ).texture;
     document.body.appendChild(this.renderer.domElement);
+  }
+
+  initOutline() {
+    this.composer = new EffectComposer(this.renderer);
+
+    const renderPass = new RenderPass(this.scene, this.camera);
+    this.composer.addPass(renderPass);
+
+    this.outlinePass = new OutlinePass( new THREE.Vector2(window.innerWidth, window.innerHeight), 
+      this.scene, this.camera);
+    this.outlinePass.edgeStrength = Number(10);
+    this.outlinePass.edgeGlow = Number(1);
+    this.outlinePass.edgeThickness = Number(1);
+    this.outlinePass.pulsePeriod = Number(5);
+    this.outlinePass.visibleEdgeColor.set("#ffffff");
+    this.outlinePass.hiddenEdgeColor.set("#000000");
+    this.composer.addPass(this.outlinePass);
+
+
+    const luminosityPass = new ShaderPass( LuminosityShader );
+    const effectFXAA = new ShaderPass(FXAAShader);
+    effectFXAA.uniforms['resolution'].value.set(1 / window.innerWidth, 1 / window.innerHeight);
+    effectFXAA.renderToScreen = true;
+    this.composer.addPass(effectFXAA);
   }
 
   loadModel() {
@@ -66,33 +107,24 @@ export default  class Index extends Vue {
     );
   }
 
+
   initCamera() {
     this.camera.position.z = 2.56;
     this.camera.position.y = 6.9;
     this.camera.position.x = 9.9; 
     this.camera.zoom = 0.3;
     this.camera.lookAt(this.room.position);
+    this.initOutline();
   }
 
   initLight() {
     const colorBackground = new THREE.Color(0x4B0082);
-    const light = new THREE.PointLight(0x6495ed, 5, 200);
-    const alight = new THREE.AmbientLight(0xffffff, 1);
-    const alight2 = new THREE.PointLight(0xffffff, 3, 200)
-    const directionalLight = new THREE.DirectionalLight( 0xffffff, 4.5 );
+    this.lightOffice = new THREE.PointLight(0x6495ed, 5, 200);
 
-    const pmremGenerator = new THREE.PMREMGenerator( this.renderer );
-    this.scene.environment = pmremGenerator.fromScene( new RoomEnvironment(), 0.04 ).texture;
-    light.position.set(0, 10, 0);
-    alight.position.set(0, 0, 0);
-    alight2.position.set(10, 10, 5);
+    this.lightOffice.position.set(6, 0, -16);
 
     this.scene.background = colorBackground;
-    //this.scene.background = new THREE.Color( 0xbfe3dd );
-   // this.scene.add(light);
-    //this.scene.add(alight);
-   // this.scene.add( directionalLight );
-    //this.scene.add(alight2);
+    this.scene.add(this.lightOffice);
   }
 
   animate() {
@@ -101,30 +133,76 @@ export default  class Index extends Vue {
         //this.room.rotation.y += 0.001;
       }
       this.controls.update();
-      this.renderer.render(this.scene, this.camera);
+      //this.renderer.render(this.scene, this.camera);
+      this.composer.render();
   }
 
   listenKeyboard() {
     document.addEventListener("keydown", (event) => {
       var keyCode = event.which;
-        if (keyCode == 38) {
-            this.room.position.x += 1;
-        } else if (keyCode == 40) {
-            this.room.position.x -= 1;
-        } else if (keyCode == 37) {
-            this.room.position.z -= 1;
-        } else if (keyCode == 39) {
-            this.room.position.z += 1;
-        } else if (keyCode == 78) {
-           this.room.position.set(0, 0, 0);
-        }
-        console.log("Room position = ");
-        console.log(this.room.position);
-        console.log("Camera position = ");
-        console.log(this.camera.position);
-        console.log("Zoom Orbit Control = ");
-        
+        // if (keyCode == 38) {
+        //     this.room.position.x += 10;
+        // } else if (keyCode == 40) {
+        //     this.room.position.x -= 10;
+        // } else if (keyCode == 37) {
+        //     this.room.position.z -= 10;
+        // } else if (keyCode == 39) {
+        //     this.room.position.z += 10;
+        // } else if (keyCode == 78) {
+        //    this.room.position.set(0, 0, 0);
+        // }
+        this.debugObject(this.lightOffice, "LightOffice", keyCode);
+       // console.log("Room position = ");
+       // console.log(this.room.position);
+        //console.log("Camera position = ");
+        //console.log(this.camera.position);
+        //console.log("Zoom Orbit Control = ");
     }, false);
+  }
+
+  debugObject(obj: any, str:any, keyCode: any) {
+    console.log(str);
+    console.log("x", obj.position.x);
+    console.log("y", obj.position.y);
+    console.log("z", obj.position.z);
+    if (keyCode == 38) {
+          obj.position.x += 2;
+        } else if (keyCode == 40) {
+            obj.position.x -= 2;
+        } else if (keyCode == 37) {
+            obj.position.z -= 2;
+        } else if (keyCode == 39) {
+            obj.position.z += 2;
+        } else if (keyCode == 78) {
+           obj.position.set(0, 0, 0);
+        }
+  }
+
+  addSelectedObject(object: any) {
+    this.selectedObjects = [];
+    this.selectedObjects.push(object);
+	}
+
+  onMouseMove(event:any) {
+	  this.mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+	  this.mouse.y = - (event.clientY / window.innerHeight) * 2 + 1;
+    this.checkIntersection();
+   // console.log("Mouse = ", this.mouse);
+  }
+
+  checkIntersection() {
+    this.raycaster.setFromCamera(this.mouse, this.camera);
+    const intersects = this.raycaster.intersectObject(this.scene, true );
+    if (!this.outlinePass) return;
+    if (intersects.length > 0) {
+      const selectedObject = intersects[0].object;
+      this.addSelectedObject( selectedObject );
+    //  console.log("Intersect")
+      this.outlinePass.selectedObjects = this.selectedObjects;
+    } else {
+      this.outlinePass.selectedObjects = [];
+    }
+
   }
 
   mounted() {
@@ -134,6 +212,7 @@ export default  class Index extends Vue {
     this.initLight();
     this.animate();
     this.listenKeyboard();
+    window.addEventListener( 'mousemove', this.onMouseMove, false );
   }
 }
 </script>
