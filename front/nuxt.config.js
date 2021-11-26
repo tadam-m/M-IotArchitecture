28219762a00e81c8253267a98be8229e5822c517
Leblanc Charles <charles.leblanc@epitech.eu>
2021-11-26 18:04:51 +0100

    [FIX] localhost to network adress listening server

--- front/nuxt.config.js
+++ front/nuxt.config.js
@@ -15,6 +15,9 @@ export default {
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
     ]
   },
+  server: {
+    host: '0' // default: localhost
+  },
 
   publicRuntimeConfig: {
     myJWTToken: process.env.JWT_TOKEN,
